75ffbb4f2449657159d340a18dcfebfb8d93ca35
Shukaaa1 <veriytb@gmail.com>
2025-08-21 21:10:44 +0700

    Create latihan_graph3.py
    ini aku mau bikin graph line biar tau growth penjualannya

---
+++ latihan_graph3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+url = 'https://raw.githubusercontent.com/sushantag9/Supermarket-Sales-Data-Analysis/master/supermarket_sales%20-%20Sheet1.csv'
+df = pd.read_csv(url)
+
+
